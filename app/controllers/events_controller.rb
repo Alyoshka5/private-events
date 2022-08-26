05533afdcda0c1711a1f1c5c206c1be266af9a20
Alyoshka <alyoshkamyshko@gmail.com>
2022-08-26 12:10:08 -0700

    Add show and new views for events and give user ability to create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,32 @@
 class EventsController < ApplicationController
-    
+    before_action :authenticate_user!, only: [:new, :create]
+
     def index
         @events = Event.all
         @users = User.all
+    end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    def new
+        @event = current_user.events.build
+    end
+
+    def create
+        @event = current_user.events.build(event_params())
+
+        if @event.save
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:name, :date, :location)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User'
+
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :date, presence: true
+    validates :location, presence: true
 end
