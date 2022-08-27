227e4a80a3cf9ab7e933766974f0f0da2deaec27
Alyoshka <alyoshkamyshko@gmail.com>
2022-08-27 12:44:36 -0700

    Add custom methods to events controller so that users can attend and unattend events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :add_attendee]
 
     def index
         @events = Event.all
@@ -22,6 +22,18 @@ class EventsController < ApplicationController
         else
             render :new, status: :unprocessable_entity
         end
+    end
+
+    def add_attendee
+        @event = Event.find(params[:id])
+        @event.attendees << current_user
+        redirect_to event_path
+    end
+
+    def remove_attendee
+        @event = Event.find(params[:id])
+        @event.attendees.destroy(current_user)
+        redirect_to event_path
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: :show
   # Defines the root path route ("/")
   root "events#index"
+  get "events/:id/add_attendee" => "events#add_attendee", as: :events_add_attendee
+  get "events/:id/remove_attendee" => "events#remove_attendee", as: :events_remove_attendee
 end
